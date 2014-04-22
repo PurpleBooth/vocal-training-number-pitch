367a21ba4da41d51a757799771f8dc849a22f77b
Billie Alice Thompson <billie@billiecodes.com>
2014-04-22 13:09:35 +0100

    Basic number + pitch

---
+++ js/main.js
@@ -0,0 +1,62 @@
+(function ($) {
+    "use strict";
+
+    function NumberGeneration() {
+        var MAX_NO_OF_UPS = 3;
+
+        var self = {};
+        var currentNumber = 1;
+        var upCount = 0;
+        var lastMovementWasUp = null;
+        var downCount = 0;
+
+        self.init = function () {
+            self.setChangeInterval();
+        };
+
+        self.setChangeInterval = function () {
+            setInterval(self.changeText, 2000);
+        };
+
+        self.changeText = function () {
+            var up;
+
+            if (upCount >= MAX_NO_OF_UPS && lastMovementWasUp) {
+                up = false;
+            }
+            else if (downCount >= MAX_NO_OF_UPS && !lastMovementWasUp) {
+                up = true
+            }
+            else {
+                up = Math.round(Math.random()) == 1;
+            }
+
+            if (up) {
+                upCount++;
+                downCount = 0;
+                lastMovementWasUp = true;
+            }
+            else {
+                downCount++;
+                upCount = 0;
+                lastMovementWasUp = false;
+            }
+
+            $('body').text(currentNumber + " " + (up ? "\u21E7" : "\u21E9")).css('color', up ? 'green' : 'blue');
+
+            if (currentNumber < 10) {
+                currentNumber++;
+            }
+            else {
+                currentNumber = 1;
+            }
+
+        };
+
+        return self;
+    }
+
+    window.numberGeneration = new NumberGeneration();
+    numberGeneration.init();
+})(jQuery);
+
